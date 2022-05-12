60f93df01f93fc457709843e64ad775e6d05c25b
Javier Delgado <javierdelgado@Javiers-MacBook-Pro.local>
2022-05-12 11:51:59 -0700

    adds css styling to cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,28 @@
 import './App.css';
-import {useRef, useState, useEffect} from "react";
+import {useState, useEffect} from "react";
 import Cards from "./components/cards/Cards";
 import Header from "./components/header/Header";
 import Form from "./components/Form";
 
 
 function App() {
+const [url, setUrl] = useState("https://swapi.dev/api/starships/?page=");
+
 const [star, setStar] = useState([]);
-const url = "https://swapi.dev/api/starships/?page=1"
+
 const getStar = async () => {
   const res = await fetch(url)
   const data = await res.json()
   setStar(data);
+  setUrl(data.next);
   console.log(data);
 }
 useEffect(() => {getStar()}, []);
 
-const inputRef = useRef(null);
-
-
-const handleSubmit = (e) => {
-  e.preventDefault();
-  const pageNum = inputRef.current.value
-  setStar(`${url}?page=${pageNum}`)
-}
-
-
 return ( 
   <div className="app">
     <Header />
-    <Form onSubmit={() => handleSubmit()}/>
+    <Form />
     <Cards star={star} />  
   </div>
   )
